d8b9ed1c9599b6d2b2217b426204009524fb1faa
greatscott <your.javelonagian@live.com>
2013-03-02 00:44:49 +0800

    hellox

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,13 +9,13 @@ SampleApp::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.server_static_assets = true
+  config.server_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
